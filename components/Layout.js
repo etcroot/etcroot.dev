1e481db39100ed80bacbdd80c99d7049b243e51f
etcroot <etcroot@protonmail.com>
2022-05-03 09:23:39 +0200

    Add favicon
    Added favicon and resources

--- components/Layout.js
+++ components/Layout.js
@@ -12,6 +12,26 @@ export default function Layout(props) {
           content="Personal landing page for etcroot, mostly socials and ways to contact me."
         />
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff"></meta>
       </Head>
       <main className="content">
         <section className="content-body">
